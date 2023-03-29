1fcb226e368d81c52d7fb2a50d5c7d2ce8c03ff2
Christian <christiancatamuscay@r-fast.com>
2023-03-29 14:52:20 -0500

    Singleton(Patrones de Creación)

---
+++ src/main/java/org/example/creational_patterns/government/Government.java
@@ -0,0 +1,16 @@
+package org.example.creational_patterns.government;
+
+public class Government {
+
+    static Government government;
+
+    private Government () { }
+
+    public synchronized static Government getGovernment(){
+        if(government == null){
+            government = new Government();
+        }
+        return government;
+    }
+
+}
