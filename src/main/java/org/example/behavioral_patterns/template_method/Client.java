7b455d045f4adf8581a4d0e861f455d9e5e47c05
Christian <christiancatamuscay@r-fast.com>
2023-04-01 16:14:59 -0500

    Template method(Patrones de Comportamiento)

---
+++ src/main/java/org/example/behavioral_patterns/template_method/Client.java
@@ -0,0 +1,11 @@
+package org.example.behavioral_patterns.template_method;
+
+public class Client {
+    public static void main(String[] args) {
+        OrderProcessTemplate netOrder = new NetOrder();
+        netOrder.processOrder(true);
+        System.out.println();
+        OrderProcessTemplate storeOrder = new StoreOrder();
+        storeOrder.processOrder(true);
+    }
+}
---
+++ src/main/java/org/example/behavioral_patterns/template_method/OrderProcessTemplate.java
@@ -0,0 +1,30 @@
+package org.example.behavioral_patterns.template_method;
+
+public abstract class OrderProcessTemplate {
+
+    public boolean isGift;
+
+    public abstract void doSelect();
+
+    public abstract void doPayment();
+
+    public abstract void doDelivery();
+
+    public final void processOrder(boolean isGift) {
+        doSelect();
+        doPayment();
+        if (isGift) {
+            giftWrap();
+        }
+        doDelivery();
+    }
+
+    public final void giftWrap() {
+        try {
+            System.out.println("Gift wrap successfull");
+        }
+        catch (Exception e) {
+            System.out.println("Gift wrap unsuccessful");
+        }
+    }
+}
